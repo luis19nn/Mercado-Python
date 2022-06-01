89aacb932a665256003c978fa9bacc979a47e9c0
luis.ferreira <luis.ferreira@smartgreen.net>
2022-06-01 15:54:15 -0300

    CHANGE and DELETE arquivos mercado.py e teste.py, respectivamente

--- mercado.py
+++ mercado.py
@@ -14,7 +14,38 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('===================================')
+    print('============ Bem-vinde ============')
+    print('============ Luis SHOP ============')
+    print('===================================')
+
+    print('Selecione uma opção abaixo: ')
+    print('1 - Cadastrar produto')
+    print('2 - Listar produto')
+    print('3 - Comprar produto')
+    print('4 - Visualizar carrinho')
+    print('5 - Fechar pedido')
+    print('6 - Sair do sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        cadastrar_produto()
+    elif opcao == 2:
+        listar_produtos()
+    elif opcao == 3:
+        comprar_produto()
+    elif opcao == 4:
+        visualizar_carrinho()
+    elif opcao == 5:
+        fechar_pedido()
+    elif opcao == 6:
+        print('Volte sempre!')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida!')
+        menu()
 
 
 def cadastrar_produto() -> None:
